8a643003b71e75b08934f03af5323b95b9bffd36
Nikita112033 <nikbel2003@gmail.com>
2021-03-26 14:55:04 +0300

    Добавлены таблица пользователей и регистрация

---
+++ data/user.py
@@ -0,0 +1,18 @@
+from .db_session import SqlAlchemyBase
+import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    username = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String)
+    password = sqlalchemy.Column(sqlalchemy.String)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,6 +1,64 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+from data import db_session
+from data.user import User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '421b1f57d3e228b7'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Адрес электронной почты', validators=[DataRequired()])
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            params = {
+                'title': 'Регистрация',
+                'form': form,
+                'message': 'Пароли не совпадают'
+            }
+            return render_template('register.html', **params)
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            params = {
+                'title': 'Регистрация',
+                'form': form,
+                'message': 'Пользователь с такой почтой уже существует'
+            }
+            return render_template('register.html', **params)
+        user = User()
+        user.username = form.username.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        # return redirect('/login')
+    params = {
+        'title': 'Регистрация',
+        'form': form,
+    }
+    return render_template('register.html', **params)
 
 
 @app.route('/')
@@ -12,6 +70,7 @@ def index():
 
 
 def main():
+    db_session.global_init('db/db.sqlite')
     app.run()
 
 
